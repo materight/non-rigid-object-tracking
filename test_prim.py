c16430e310a5315c25f83b0c32a9425c8d4e7897
materight <matteo.est@gmail.com>
2021-02-23 21:49:18 +0100

    Update prim test script

--- test_prim.py
+++ test_prim.py
@@ -2,23 +2,34 @@ from prim import RP
 import numpy as np
 import cv2 as cv
 import skimage.segmentation as skiseg
+from benchmark import computeBenchmark
 
 img = cv.imread('frog.png')
+truth = cv.imread('frog_truth.png', 0)
 
 segments = skiseg.quickshift(img, kernel_size=3, max_dist=6, ratio=0.5)
+# segments = skiseg.felzenszwalb(img, scale=100, sigma=0.5, min_size=20)
 
 res = RP(img, 1000, segment_mask=segments)
 
 def plt():
-	for i in np.random.choice(range(res.shape[0]), size=100):
+	# r = cv.selectROI(img)
+	# bbox = np.zeros(img.shape[:2], dtype=np.uint8)
+	# bbox[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])] = 1
+	dist = np.array([-np.sum(truth.astype(np.bool).reshape(-1) & res[i].reshape(-1)) / np.sum(truth.astype(np.bool).reshape(-1) | res[i].reshape(-1)) for i in range(res.shape[0])])
+	segs = dist.argsort()
+	for i in segs:
 		masked = img.copy()
 		masked[~res[i]] = [0, 0, 0]
 		masked = skiseg.mark_boundaries(masked, segments, color=(0, 0, 0))
 		cv.imshow('image', masked)
-		cv.waitKey(0)
+		if cv.waitKey(0) & 0xFF == ord('q'): 
+			break
 		cv.destroyAllWindows()
+	cv.destroyAllWindows()
 
 def plt_segs():
-	cv.imshow('segments', skiseg.mark_boundaries(img, segments))
+	mask = skiseg.mark_boundaries(img, segments)
+	cv.imshow('segments', mask)
 	cv.waitKey(0)
 	cv.destroyAllWindows()
