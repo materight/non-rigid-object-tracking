1020c2955d9a9affba3119bdb14e4b427bad253d
steveazzolin <steve.azzolin1@gmail.com>
2021-02-23 15:45:36 +0100

    - LinPau beta

--- maskers/lin_pun_tracker.py
+++ maskers/lin_pun_tracker.py
@@ -10,7 +10,7 @@ from sklearn.svm import SVC
 from sklearn.decomposition import PCA
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, jaccard_score
 
 from skimage.segmentation import slic, quickshift, mark_boundaries
 
@@ -26,103 +26,157 @@ class LinPauNonRigidTracker(Masker):
         Masker.__init__(self, **args)
 
         self.ground_truth = None
-        self.poly_roi = poly_roi
+        self.prev_target = None
+
+        self.rho = 0.1 #for an image smaller than 600x400
+        self.num_level_erosion = 3
+        self.erosion_kernel_size = 2
+
         self.index = 0
 
-        if self.poly_roi:  # convert the list of points into a binary map
-            """for i in range(len(self.poly_roi)):  # adapt coordinates
-                x = self.poly_roi[i][0]
-                y = self.poly_roi[i][1]
-                self.poly_roi[i] = (x - self.prevBbox[0], y - self.prevBbox[1])"""
-
+        if poly_roi:  # convert the list of points into a binary map
             self.ground_truth = np.zeros([self.prevFrame.shape[0], self.prevFrame.shape[1]], dtype=np.uint8)
-            cv.fillPoly(self.ground_truth, np.array([self.poly_roi], dtype=np.int32), 255)
+            cv.fillPoly(self.ground_truth, np.array([poly_roi], dtype=np.int32), 255)
+            self.ground_truth = self.ground_truth.astype(np.bool)
 
 
-    def update(self, bbox, frame, mask, color):
-        """
-        Requires the very first frame as input here
-        """
+    def update(self, frame):
+        #segments_quick = quickshift(frame, kernel_size=3, max_dist=6, ratio=0.5, random_seed=42)
+        segments_quick = slic(frame)
+        prob_map = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
 
-        segments_quick = quickshift(frame, kernel_size=3, max_dist=6, ratio=0.5, random_seed=42)
-        #plt.imshow(segments_quick)
-        #plt.show()
-
-        
-
+        if self.index == 5:
+            return
 
         if self.index == 0:
             labels_foreground = self.getForegroundSegments(segments_quick, self.ground_truth, frame)
             X , y = self.extractFeatures(frame, segments_quick, labels_foreground)
 
-
+            self.knn = RandomForestClassifier(random_state=42, n_estimators=3, max_depth=7).fit(X,y) #RadiusNeighborsClassifier(n_jobs=2 ,radius=0.05, weights='distance', outlier_label="most_frequent").fit(X,y) #RandomForestClassifier(random_state=42, n_estimators=55, max_depth=13).fit(X,y) #KNeighborsClassifier(n_neighbors=k, weights='distance').fit(X, y)
             
-            crop_frame = frame[self.prevBbox[1]:self.prevBbox[1] + self.prevBbox[3], self.prevBbox[0]:self.prevBbox[0] + self.prevBbox[2]]
-        
-            assert (self.poly_roi is None) or (crop_frame.shape[:2] == self.mask.shape)
-            #X , y = self.getRGBFeatures(crop_frame)
-            X , y = self.getRGBFeaturesWithNeighbors(crop_frame, bbox, train=True)
-            X_nroi , y_nroi = self.getRONI(frame)
-
-            X = np.concatenate([X, X_nroi], axis=0)
-            y = np.concatenate([y, y_nroi])
-
-            k = 5
-            self.scaler = StandardScaler()
-            X = X / 255 
-            self.knn = RadiusNeighborsClassifier(n_jobs=2 ,radius=0.05, weights='distance', outlier_label="most_frequent").fit(X,y) #RandomForestClassifier(random_state=42, n_estimators=55, max_depth=13).fit(X,y) #KNeighborsClassifier(n_neighbors=k, weights='distance').fit(X, y)
-            self.train_X = X; self.train_y = y
-            print(self.knn.outlier_label_)
             y_pred = self.knn.predict(X)
             probs = self.knn.predict_proba(X)
             f1 = round(f1_score(y, y_pred), 2)
             print("F1 score classifier = ", f1)
+            
+            for i , label in enumerate(np.concatenate([labels_foreground, np.setdiff1d(np.unique(segments_quick), labels_foreground)]) ):
+                prob_map[segments_quick == label] = probs[i, 1] * 255
 
-            prob_map = np.zeros_like(self.mask, dtype=np.int16)
-            mask_active_idx = np.argwhere(self.mask > 0)
-            mask_deactive_idx = np.argwhere(self.mask == 0)
+            self.prev_target = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
+            for l in labels_foreground:
+                self.prev_target[segments_quick == l] = 255
 
-            for i , idx in enumerate(mask_active_idx):
-                prob_map[idx[0], idx[1]] = probs[i, 1] * 255  
-
-            start = sum(y==1)
-            for i , idx in enumerate(mask_deactive_idx):
-                prob_map[idx[0], idx[1]] = probs[start + i, 1]  * 255
-
-            """fig = plt.figure()
-            #ax = fig.add_subplot(111, projection='3d')
-            #ax.scatter(X[y == 1, 0], X[y == 1, 1], X[y == 1, 2])
-            #ax.scatter(X[y == 0, 0], X[y == 0, 1], X[y == 0, 2])
-            ax.set_title('KNN F1 = {} with K={}'.format(f1, k))
-            plt.show()"""
-
-            plt.imshow(cv.blur(prob_map,(2,2)), cmap='hot')
+            plt.imshow(prob_map, cmap='hot')
             plt.show()
         else:
-            crop_frame = frame[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]]
-            X , _ = self.getRGBFeaturesWithNeighbors(crop_frame, bbox, train=False)
-            X = X / 255 
-
+            X , _ = self.extractFeatures(frame, segments_quick, labels_foreground=[])
             probs = self.knn.predict_proba(X)
-            prob_map = np.zeros_like(crop_frame, dtype=np.uint8)
-            c = 0
-            for i in range(prob_map.shape[0]):
-                for j in range(prob_map.shape[1]):
-                    prob_map[i, j] = 255  if probs[c, 1] >= 0.6 else 0
-                    c += 1 #TODO: infer c from i and j
-            cv.imshow("prob", cv.morphologyEx(prob_map, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))))
-            mask[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]] = prob_map[:,:,0]
-            #plt.imshow(cv.blur(prob_map,(2,2)), cmap='hot')
-            #plt.show()
+
+            for i , label in enumerate(np.unique(segments_quick)):
+                prob_map[segments_quick == label] = probs[i, 1] * 255
+
+            candidates = self.getCandidatesBBox(frame, segments_quick) 
+
+
+            _ , areas = np.unique(segments_quick, return_counts=True)
+            similarity = [] #similarity for every candidate. Eq. (2)
+            for candidate in candidates:
+                tmp = 0
+                segments = np.unique(candidate)
+                for segment in segments:
+                    tmp += np.tan(probs[segment, 1]) * areas[segment]
+                tmp /= len(segments)
+                similarity.append(tmp) #TODO: compute b
+            
+            motion_weights = [] #Eq. (3)
+            for candidate in candidates:
+                candidate_binary = np.zeros_like(candidate, dtype=np.bool)
+                candidate_binary[candidate > 0] = True #create binary candidate from segmented candidate
+                
+                intersection = np.logical_and(candidate_binary.reshape(-1), self.prev_target.astype(np.bool).reshape(-1))
+                union = np.logical_or(candidate_binary.reshape(-1), self.prev_target.astype(np.bool).reshape(-1))
+                iou = np.sum(intersection) / np.sum(union)
+
+                center_distance = np.exp(-self.rho * self.computeDistanceFromCenters(candidate, self.prev_target))
+
+                motion_weights.append(iou + center_distance)
+
+            similarity = np.array(similarity)
+            similarity /= max(similarity) #normalize. Equal to compute b in Eq (2)
+            motion_weights = np.array(motion_weights)
+            best_candidate = np.argmax(similarity * motion_weights)
+            self.prev_target = candidates[best_candidate]
+
+            cv.imshow("Best candidate", candidates[best_candidate])
+            print(sum(sum(self.prev_target)))
 
         self.index += 1
 
 
+    def getCandidatesBBox(self, frame, segmentation, strategy="all", proposal_box_limit = 100, print_results=False):
+        """
+        Selective search for candidate proposal. 
+        Variation wrt the paper. 
+        """
+        ss = cv.ximgproc.segmentation.createSelectiveSearchSegmentation()
+        # Convert image from BGR (default color in OpenCV) to RGB
+        rgb_im = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        ss.addImage(rgb_im)
+        gs = cv.ximgproc.segmentation.createGraphSegmentation()
+        ss.addGraphSegmentation(gs)
+
+        if strategy == "color": # Creating strategy using color similarity
+            strategy_color = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyColor()
+            ss.addStrategy(strategy_color)        
+        else: # Creating strategy using all similarities (size,color,fill,texture)
+            strategy_color = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyColor()
+            strategy_fill = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyFill()
+            strategy_size = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategySize()
+            strategy_texture = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyTexture()
+            strategy_multiple = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyMultiple(strategy_color, strategy_fill, strategy_size, strategy_texture)
+            ss.addStrategy(strategy_multiple)
+
+        get_boxes = ss.process()
+        print("Total proposals = ", len(get_boxes))
+        boxes = [[box[0], box[1], box[0] + box[2], box[1] + box[3]] for box in get_boxes[0:proposal_box_limit]]
+
+        #create mask in which segments belonging to the bbox are set to the value of the segment's label, the others are set to zero
+        ret = []
+        for bbox in boxes:
+            candidate = np.zeros_like(segmentation, dtype=np.uint8)
+
+            mask = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.bool)
+            mask[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]] = True
+
+            labels_in_bbox = self.getForegroundSegments(segmentation, mask, frame)
+            for label in labels_in_bbox:
+                candidate[segmentation == label] = label
+            #cv.imshow("ret", candidate)
+            #cv.waitKey()
+            ret.append(candidate)
+        
+        if print_results:
+            output_img_proposal_top100 = frame.copy()
+            # Draw bounding boxes for top 100 proposals
+            for i in range(0, len(boxes)):
+                if i == 30:
+                    top_x, top_y, width, height = boxes[i]
+                    cv.rectangle(output_img_proposal_top100, (top_x, top_y), (top_x + width, top_y + height), (0, 255, 0), 1, cv.LINE_AA)
+            cv.imshow("Output_Top_100_Proposals", output_img_proposal_top100)
+            #cv.waitKey()
+            cv.destroyAllWindows()
+
+        return ret
+
+
     def extractFeatures(self, frame, segments, labels_foreground, print_results=False):
+        """
+        Extract the HHH feature as proposed by the paper, i.e, HSV histogram for a sequence of eroded segments
+        """
         X , y = [] , []
-        kernel = np.ones((2,2),np.uint8) #erosion kernel. Size not specified by the paper
+        kernel = np.ones((self.erosion_kernel_size, self.erosion_kernel_size),np.uint8) #erosion kernel. Size not specified by the paper
+        num_level_erosion = self.num_level_erosion
         eps = 0.00000001
-        num_level_erosion = 3
         
         #foreground
         for f_label in labels_foreground:
@@ -176,129 +230,44 @@ class LinPauNonRigidTracker(Masker):
         Partially contained segments are assigned to foreground if they belong for at least 70%
         to the ground truth
         """
-        assignements = defaultdict(float)
-        for i in range(segments.shape[0]):
-            for j in range(segments.shape[1]):
-                assignements[segments[i,j]] += 1 if ground_truth[i,j] == 255 else 0
+        labels , counts = np.unique(segments[ground_truth == True], return_counts=True)
+        assignements = dict(zip(labels,counts)) 
         
         #normalize by the area
         _ , counts = np.unique(segments, return_counts=True)
         for key in assignements.keys():
-            assignements[key] /= counts[key]
+            assignements[key] /= counts[key]                
         
-        #show result
-        test = np.zeros_like(segments, dtype=np.uint8)
-        for i in range(segments.shape[0]):
-            for j in range(segments.shape[1]):
-                test[i,j] = 255 if assignements[segments[i,j]] >= 0.7 else 0 #assignements[segments[i,j]] * 255
+        #to show the results
+        #test = np.zeros_like(segments, dtype=np.uint8)
+        #for i in range(segments.shape[0]):
+        #    for j in range(segments.shape[1]):
+        #        test[i,j] = 255 if assignements[segments[i,j]] >= 0.7 else 0 #assignements[segments[i,j]] * 255
 
-        #plt.imshow(mark_boundaries(cv.cvtColor(frame, cv.COLOR_BGR2GRAY), segments))
-        #plt.show()
-        show_image = cv.addWeighted(src1=cv.cvtColor(frame, cv.COLOR_BGR2GRAY), alpha=0.7, src2=test, beta=0.3, gamma=0)
-        cv.imshow("Foreground superpixels", show_image)
+        #show_image = cv.addWeighted(src1=cv.cvtColor(frame, cv.COLOR_BGR2GRAY), alpha=0.1, src2=test, beta=0.9, gamma=0)
+        #boundaries = mark_boundaries(show_image, segments)
+        #bbox_gt = np.zeros_like(frame)
+        #bbox_gt[:,:,2] = ground_truth
+        #cv.imshow("Superpixels", boundaries + bbox_gt) 
+        #cv.waitKey()
+        #cv.imshow("Foreground superpixels", show_image)
 
         return np.array(list(assignements.keys()))[np.array(list(assignements.values())) >= 0.7]
 
 
-
-    def getRONI(self, frame):
+    def computeDistanceFromCenters(self, mask, truth):
         """
-        Select Region of Non-Interest (area that surely belongs to the background). 
-        Augment the dataset of feature vector with more negative (background) samples, to increase (maybe) the discriminative power of the 
-        classifier
+        Compute the Euclidean distance between the center of mass of the two masks
         """
-        bbox = cv.selectROI('Select one RONI', frame, False)
-        crop_frame = frame[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]]
-        X , y = self.getRGBFeaturesWithNeighbors(crop_frame, bbox, train=False)
-        return X , y
+        mm = cv.moments(mask)
+        tm = cv.moments(truth)
 
+        if np.any(mask > 0):
+            mCenter = int(mm["m10"] / mm["m00"]), int(mm["m01"] / mm["m00"])
+            tCenter = int(tm["m10"] / tm["m00"]), int(tm["m01"] / tm["m00"])
+            dist = ((mCenter[0] - tCenter[0])**2 +  (mCenter[1] - tCenter[1])**2)**.5
+        else:
+            print('Warning: empty mask')
+            dist = max(mask.shape[0], mask.shape[1]) #very high distance
+        return dist
 
-    def getRGBFeatures(self, crop_frame):
-        #extract foreground
-        B = crop_frame[:,:,0][self.mask >= 0] 
-        G = crop_frame[:,:,1][self.mask >= 0] 
-        R = crop_frame[:,:,2][self.mask >= 0] 
-        
-        X = np.stack([B,G,R], axis=1)
-        y = np.ones(G.shape[0])
-
-        #extract background
-        B = crop_frame[:,:,0][self.mask == 0] 
-        G = crop_frame[:,:,1][self.mask == 0] 
-        R = crop_frame[:,:,2][self.mask == 0]
-        
-        X = np.concatenate([X, np.stack([B, G, R], axis=1)], axis=0)
-        y = np.concatenate([y, np.zeros(G.shape[0])])
-
-        """fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        r_vals , g_vals , b_vals , c_vals = [] , [] , [] , [] 
-        for i in range(len(G)):
-            r_vals.append(R[i]) 
-            g_vals.append(G[i])
-            b_vals.append(B[i])
-            c_vals.append([R[i], G[i], B[i]])
-        ax.scatter(r_vals, g_vals, b_vals, c=np.array(c_vals)/255)
-        plt.show()"""
-
-        return X , y
-
-    def getRGBFeaturesWithNeighbors(self, crop_frame, bbox, train=False):
-        """
-        Return RGB values of the 4-neighboorood along with the central pixel's values
-        """
-        #crop_frame = cv.cvtColor(crop_frame, cv.COLOR_BGR2LAB)
-        X_pos , X_neg , y_pos , y_neg = [] , [] , [] , []
-        for i in range(crop_frame.shape[0]):
-            for j in range(crop_frame.shape[1]):
-                features = crop_frame[i,j].tolist()
-                for span in [(-1,0), (+1,0), (0,-1), (0,+1),(+1,+1) , (-1,-1), (+1,-1), (-1,+1),
-                             (-2,0), (+2,0), (0,-2), (0,+2),(+2,+2) , (-2,-2), (+2,-2), (-2,+2),
-                             (-3,0), (+3,0), (0,-3), (0,+3),(+3,+3) , (-3,-3), (+3,-3), (-3,+3)]:
-                    neighbor = (i + span[0] , j + span[1])
-                    if (neighbor[0] >= bbox[1] and neighbor[0] <= bbox[1] + bbox[3] and
-                    neighbor[1] >= bbox[0] and neighbor[1] <= bbox[0] + bbox[2]):
-                        features.extend(crop_frame[neighbor[0], neighbor[1]].tolist())
-                    else:
-                        features.extend([-1, -1, -1])
-                        
-                if train and self.mask[i,j] > 0:
-                    y_pos.append(1)
-                    X_pos.append(features)
-                else:
-                    y_neg.append(0)
-                    X_neg.append(features)
-        
-        X = X_pos + X_neg
-        y = y_pos + y_neg
-        X = np.array(X, dtype=np.int16)
-        y = np.array(y, dtype=np.int8)
-        return X , y
-
-"""
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-    def exp(self, bbox, frame, point1_t, point2_t, point1_k, point2_k, color):
-        crop_frame = frame[self.prevBbox[1]:self.prevBbox[1] + self.prevBbox[3], self.prevBbox[0]:self.prevBbox[0] + self.prevBbox[2]]
-        kp2, des2 = self.orb.detectAndCompute(crop_frame, mask=None)
-        des_dentro , kp_dentro , des_fuori , kp_fuori = self.filterFeaturesByMask(kp2, des2, None)
-
-        X = []
-        for a in des_dentro:
-            X.append(a)
-        for a in des_fuori:
-            X.append(a)
-        
-        X = np.array(X)
-        y = np.concatenate([np.ones(len(des_dentro)), np.zeros(len(des_fuori))])
-
-        
-        scaler = StandardScaler()
-        X_pca = PCA(n_components=3).fit_transform(scaler.fit_transform(X))
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(X_pca[:len(des_dentro),0] , X_pca[:len(des_dentro),1], X_pca[:len(des_dentro),2])
-        ax.scatter(X_pca[len(des_dentro):,0] , X_pca[len(des_dentro):,1], X_pca[len(des_dentro):,2])
-        plt.show()
-"""
