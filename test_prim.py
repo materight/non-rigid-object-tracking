cb026fb0c71f18029f63129dbbc2d711d1bee239
materight <matteo.est@gmail.com>
2021-02-26 15:10:31 +0100

    Added custom colorspace in graph.h

--- test_prim.py
+++ test_prim.py
@@ -4,14 +4,29 @@ import cv2 as cv
 import skimage.segmentation as skiseg
 from benchmark import computeBenchmark
 
-IMAGE = 'frog'
+IMAGE = 'worm'
 img = cv.imread(f'{IMAGE}.png')
 truth = cv.imread(f'{IMAGE}_truth.png', 0)
+
+pts, _ = cv.findContours(truth, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+bbox = np.array(cv.boundingRect(pts[0]))
+wpd, hpd = bbox[3] * 0.3, bbox[2] * 0.3
+bbox[1], bbox[0] = int(bbox[1] - wpd), int(bbox[0] - hpd)
+bbox[3], bbox[2] = int(bbox[3] + wpd * 2), int(bbox[2] + hpd * 2)
+
+img = img[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
+truth = truth[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
+
+print('Computing segments...')
 
 segments = skiseg.quickshift(img, kernel_size=3, max_dist=6, ratio=0.5)
 # segments = skiseg.felzenszwalb(img, scale=100, sigma=0.5, min_size=20)
 
+print('Segmentation done, computing RP...')
+
 res = RP(img, 1000, segment_mask=segments)
+
+print('Done')
 
 def plt():
     dist = np.array([-np.sum(truth.astype(np.bool) & res[i].astype(np.bool)) / np.sum(truth.astype(np.bool) | res[i].astype(np.bool)) for i in range(res.shape[0])])
