093ca20db5661b370c23d5edccb48146b300c239
steveazzolin <steve.azzolin1@gmail.com>
2021-03-23 23:10:46 +0100

    Deleted Sparse and Rigid maskers

--- maskers/__init__.py
+++ maskers/__init__.py
@@ -1,21 +1,15 @@
 import cv2 as cv
 import numpy as np
 
-from .sparse_non_rigid_masker import SparseNonRigidMasker
 from .optical_flow_masker import OpticalFlowMasker
-from .rigid_masker import RigidMasker
 from .bg_subtractor_masker import BackgroundSubtractorMasker
 from .semi_supervised_masker import SemiSupervisedNonRigidMasker
 from .lin_pun_tracker import LinPauNonRigidTracker 
 from .grab_cut import GrabCut
 
 def getMaskerByName(name, **args):
-    if name == "Sparse" or name == "SparseNonRigidMasking":
-        return SparseNonRigidMasker(**args)
     if name == "OpticalFlow":
         return OpticalFlowMasker(**args)
-    if name == "Rigid":
-        return RigidMasker(**args)
     if name == "BgSub":
         return BackgroundSubtractorMasker(**args)
     if name == "SemiSupervised":
