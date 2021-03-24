cbe291ee54d3e76775754047efc01be2b762a20a
materight <matteo.est@gmail.com>
2021-03-24 22:48:37 +0100

    Reenabled superpixel merge in OpticalFlowMasker

--- maskers/optical_flow_masker.py
+++ maskers/optical_flow_masker.py
@@ -14,7 +14,6 @@ class OpticalFlowMasker(Masker):
         featureMask = np.zeros_like(grayFrame)
         cv.fillPoly(featureMask, np.array([poly_roi], dtype=np.int32), 255)
         self.featurePoints = self.computeFeatures(frame, grayFrame, featureMask)
-        print(self.featurePoints.dtype)
         # Print extracted points
         for p in self.featurePoints:
             x, y = p.ravel()
@@ -22,6 +21,9 @@ class OpticalFlowMasker(Masker):
 
         self.prevFrame = grayFrame
         self.index = 0
+
+    def addModel(self, **args):
+        pass
 
     def computeFeatures(self, frame, grayFrame, mask):
         # return cv.goodFeaturesToTrack(grayFrame, mask=mask, maxCorners=500, qualityLevel=0.2, minDistance=2, blockSize=2)
@@ -43,13 +45,22 @@ class OpticalFlowMasker(Masker):
         convexHull = cv.convexHull(self.featurePoints)
         convexmask = np.zeros(frame.shape[:2], dtype=np.uint8)
         cv.fillPoly(convexmask, np.array([convexHull], dtype=np.int32), 255)
-        '''
+        #for point in self.featurePoints:
+        #    x, y = point.ravel()
+        #    convexmask[int(y), int(x)] = 255
+
+        bmask = np.zeros(frame.shape[:2], dtype=np.uint8)
+        bmask[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])] = convexmask[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])]
+
         # Compute superpixels and merge the mask
-        OCCUPANCY_THRESHOLD = 0.9 # Perc of occupied pixels in a segemtn to consider the segment part of the mask
+        OCCUPANCY_THRESHOLD = 0.8 # Perc of occupied pixels in a segemtn to consider the segment part of the mask
         r = max(bbox[2], bbox[3]) * 0.2
         box2 = np.array([bbox[0] - r, bbox[1] - r, bbox[2] + 2*r, bbox[3] + 2*r], dtype=np.int)
         bmask = np.zeros(frame.shape[:2], dtype=np.uint8)
-        bboxseg = skiseg.quickshift(frame[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])], kernel_size=3, max_dist=6, ratio=0.5) + 1 # S.t. id=0 is the background
+        bboxseg = skiseg.slic(frame[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])], n_segments=100, compactness=10, sigma=1, start_label=0) + 1 # S.t. id=0 is the background
+        
+        cv.imshow('superpixels', skiseg.mark_boundaries(frame[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])], bboxseg))
+        
         segments = np.zeros(frame.shape[:2], dtype=np.uint8)
         segments[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])] = bboxseg
         for seg in np.unique(segments):
@@ -58,20 +69,19 @@ class OpticalFlowMasker(Masker):
                 bmaskInSeg = len(segments[(segments == seg) & (convexmask > 0)])
                 occupancy = bmaskInSeg / area 
                 if occupancy > OCCUPANCY_THRESHOLD:
+                #if bmaskInSeg > 0:
                     bmask[segments == seg] = 255
-        '''
-        bmask = np.zeros(frame.shape[:2], dtype=np.uint8)
-        bmask[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])] = convexmask[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])]
+        
+        # Improve results with morphology opening and closing
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
+        bmask = cv.morphologyEx(bmask, cv.MORPH_DILATE, kernel)
 
-
-        '''
         # Recompute tracked points every 10 frames
         if self.index % 10 == 0:
             trackingMask = np.zeros_like(grayFrame)
             trackingMask[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])] = 255
-            self.featurePoints = cv.goodFeaturesToTrack(grayFrame, mask=trackingMask, maxCorners=500, qualityLevel=0.2, minDistance=2, blockSize=7)
-        '''
-
+            self.featurePoints = self.computeFeatures(frame, grayFrame, trackingMask)
+            
         # Draw new tracked points
         mask[:,:,2] = bmask
         
