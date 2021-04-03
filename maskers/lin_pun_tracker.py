6825cc4033a0ac474e2e181781ebf2e1fbc00f70
steveazzolin <steve.azzolin1@gmail.com>
2021-04-03 14:22:30 +0200

    Removed useless comments

--- maskers/lin_pun_tracker.py
+++ maskers/lin_pun_tracker.py
@@ -21,6 +21,7 @@ import math
 class LinPauNonRigidTracker(Masker):
     """
     Implementation of https://www.researchgate.net/publication/302065250_Highly_non-rigid_video_object_tracking_using_segment-based_object_candidates
+    Not fully tested due to poor intermediate results
     """
 
     def __init__(self, poly_roi=None, update_mask=None, **args):
@@ -29,7 +30,7 @@ class LinPauNonRigidTracker(Masker):
         self.ground_truth = None
         self.prev_target = None
 
-        self.rho = 0.1 #for an image smaller than 600x400
+        self.rho = 0.1
         self.num_level_erosion = 3
         self.erosion_kernel_size = 2
 
@@ -44,12 +45,7 @@ class LinPauNonRigidTracker(Masker):
     def update(self, frame, mask):
         #segments_quick = quickshift(frame, kernel_size=3, max_dist=6, ratio=0.5, random_seed=42)
         segments_quick = felzenszwalb(frame, scale=100, sigma=0.5, min_size=50)
-        #gradient = sobel(rgb2gray(frame))
-        #segments_quick = watershed(gradient, markers=250, compactness=0.001)
         #segments_quick = slic(frame)
-        
-        #plt.imshow(segments_quick, cmap='hot')
-        #plt.show()
 
         if self.index == 0:
             prob_map = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
@@ -69,34 +65,13 @@ class LinPauNonRigidTracker(Masker):
             self.prev_target = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
             for l in labels_foreground:
                 self.prev_target[segments_quick == l] = 255
-
-            #plt.imshow(prob_map, cmap='hot')
-            #plt.show()
         else:
             X , _ = self.extractFeatures(frame, segments_quick, labels_foreground=[])
             probs = self.knn.predict_proba(X)
 
             labels , areas = np.unique(segments_quick, return_counts=True)
-            #prob_map = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.uint8)
-            #for i , label in enumerate(labels):
-            #    prob_map[segments_quick == label] = probs[i, 1] * 255
-            #plt.imshow(prob_map, cmap='hot') 
-            #plt.show()
 
-            #candidates = self.getCandidatesBBox(frame, segments_quick) 
             candidates = self.getPrimCandidates(frame, segments_quick).astype(np.uint16)
-            """
-            temporary patch
-            
-            c = []
-            for candidate in candidates:
-                tmp2 = np.zeros(candidate.shape, dtype=np.uint8)
-                tmp2[np.nonzero(candidate == True)] = segments_quick[np.nonzero(candidate == True)]
-                c.append(tmp2)
-            candidates = c
-            """
-
-            #TODO: Fix label of segmets starting at 0
 
             similarity = [] #similarity for every candidate. Eq. (2)
             motion_weights = [] #Eq. (3)
@@ -126,74 +101,14 @@ class LinPauNonRigidTracker(Masker):
             sa = np.argsort(similarity * motion_weights)[-3:][::-1]
             for i in range(3):
                 cv.imshow(f"Best candidate {i}", candidates[sa[i]].astype(np.uint8))
-            #cv.waitKey()
-
             self.prev_target = candidates[best_candidate]
             mask[:,:, 2] = candidates[best_candidate]
-
-            #cv.imshow("Best candidate", candidates[best_candidate])
-
         self.index += 1
 
 
     def getPrimCandidates(self, frame, segments):
-        res = RP(frame, 500, segment_mask=segments)
+        res = RP(frame, 200, segment_mask=segments)
         return res
-
-
-    def getCandidatesBBox(self, frame, segmentation, strategy="all", proposal_box_limit = 100, print_results=False):
-        """
-        Selective search for candidate proposal. 
-        Variation wrt the paper. 
-        """
-        ss = cv.ximgproc.segmentation.createSelectiveSearchSegmentation()
-        # Convert image from BGR (default color in OpenCV) to RGB
-        rgb_im = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        ss.addImage(rgb_im)
-        gs = cv.ximgproc.segmentation.createGraphSegmentation()
-        ss.addGraphSegmentation(gs)
-
-        if strategy == "color": # Creating strategy using color similarity
-            strategy_color = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyColor()
-            ss.addStrategy(strategy_color)        
-        else: # Creating strategy using all similarities (size,color,fill,texture)
-            strategy_color = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyColor()
-            strategy_fill = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyFill()
-            strategy_size = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategySize()
-            strategy_texture = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyTexture()
-            strategy_multiple = cv.ximgproc.segmentation.createSelectiveSearchSegmentationStrategyMultiple(strategy_color, strategy_fill, strategy_size, strategy_texture)
-            ss.addStrategy(strategy_multiple)
-
-        get_boxes = ss.process()
-        print("Total proposals = ", len(get_boxes))
-        boxes = [[box[0], box[1], box[0] + box[2], box[1] + box[3]] for box in get_boxes[0:proposal_box_limit]]
-
-        #create mask in which segments belonging to the bbox are set to the value of the segment's label, the others are set to zero
-        ret = []
-        for bbox in boxes:
-            candidate = np.zeros_like(segmentation, dtype=np.uint8)
-
-            mask = np.zeros((frame.shape[0], frame.shape[1]), dtype=np.bool)
-            mask[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]] = True
-
-            labels_in_bbox = self.getForegroundSegments(segmentation, mask, frame)
-            for label in labels_in_bbox:
-                candidate[segmentation == label] = label
-            #cv.imshow("ret", candidate)
-            #cv.waitKey()
-            ret.append(candidate)
-        
-        if print_results:
-            output_img_proposal_top100 = frame.copy()
-            # Draw bounding boxes for top 100 proposals
-            for i in range(0, len(boxes)):
-                top_x, top_y, width, height = boxes[i]
-                cv.rectangle(output_img_proposal_top100, (top_x, top_y), (top_x + width, top_y + height), (0, 255, 0), 1, cv.LINE_AA)
-            cv.imshow("Output_Top_100_Proposals", output_img_proposal_top100)
-            #cv.waitKey()
-            cv.destroyAllWindows()
-
-        return ret
 
 
     def extractFeatures(self, frame, segments, labels_foreground, print_results=False):
@@ -251,7 +166,6 @@ class LinPauNonRigidTracker(Masker):
 
             X.append(segment_feature)
             y.append(0)
-
         X , y = np.array(X) , np.array(y, dtype=np.uint8)
         return X , y
 
@@ -269,22 +183,7 @@ class LinPauNonRigidTracker(Masker):
         #normalize by the area
         _ , counts = np.unique(segments, return_counts=True)
         for key in assignements.keys():
-            assignements[key] /= counts[key]                
-        
-        #to show the results
-        #test = np.zeros_like(segments, dtype=np.uint8)
-        #for i in range(segments.shape[0]):
-        #    for j in range(segments.shape[1]):
-        #        test[i,j] = 255 if assignements.get(segments[i,j], 0) >= 0.7 else 0 #assignements[segments[i,j]] * 255
-
-        #show_image = cv.addWeighted(src1=cv.cvtColor(frame, cv.COLOR_BGR2GRAY), alpha=0.1, src2=test, beta=0.9, gamma=0)
-        #boundaries = mark_boundaries(show_image, segments)
-        #bbox_gt = np.zeros_like(frame)
-        #bbox_gt[:,:,2] = ground_truth
-        #cv.imshow("Superpixels", boundaries + bbox_gt) 
-        #cv.waitKey(0)
-        ##cv.imshow("Foreground superpixels", show_image)
-
+            assignements[key] /= counts[key]    
         return np.array(list(assignements.keys()))[np.array(list(assignements.values())) >= 0.7]
 
 
